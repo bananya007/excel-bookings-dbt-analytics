9108db61da499d664f2fc437daf64926374eb49e
bananya <bpgayathriananya@gmail.com>
2026-07-29 17:43:40 -0500

    deduplicate bookings and add representative IDs

--- models/staging/stg_bookings.sql
+++ models/staging/stg_bookings.sql
@@ -18,7 +18,34 @@ cleaned as (
         cast("Amount (Local)" as number(18, 2)) as amount_local,
         upper(trim("Currency")) as currency 
     from source 
+),
+with_rep_id as (
+    select 
+        c.*,
+        r.rep_id
+    from cleaned c 
+    left join {{ ref('stg_rep_master') }} r 
+    on c.sales_rep = r.alias_name 
+),
+deduped as (
+    select 
+        *,
+        row_number() over (
+            partition by booking_id
+            order by booking_date desc 
+        ) as row_number 
+    from with_rep_id 
 )
-
-select *
-from cleaned 
+select 
+    booking_id,
+    booking_date,
+    customer,
+    region,
+    product,
+    deal_type,
+    sales_rep,
+    rep_id,
+    amount_local,
+    currency
+from deduped 
+where row_number = 1
